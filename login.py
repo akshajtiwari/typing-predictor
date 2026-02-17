8fb09848243b6d8ba00af844138baf7eb5466ab1
Akshaj Tiwari <akshajtiwari9a@gmail.com>
2026-02-17 11:33:26 +0530

    finalizing the export csv functionality

--- login.py
+++ login.py
@@ -3,35 +3,72 @@ import selenium.webdriver as webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
-load_dotenv()
+from selenium.webdriver import ChromeOptions
+import time
+import os
+options=webdriver.ChromeOptions()
+options.add_experimental_option("prefs",{
+
+    "download.default_directory":r"/home/akshajtiwari/Desktop/typing_predictor/downloaded_files", # linux style path
+    "download.prompt_for_download":False,
+    "download.directory_upgrade":True,
+    "safebrowsing.enabled":True
+    })
+driver=webdriver.Chrome(options=options)
+
 def login():
-    driver=webdriver.Chrome()
+    load_dotenv()
     driver.get("https://monkeytype.com/login")
     wait = WebDriverWait(driver, 15)
     # click accept cookies
     try:
         accept_btn = wait.until(EC.element_to_be_clickable((By.XPATH, "//button[contains(., 'accept')]")))
         driver.execute_script("arguments[0].click();",accept_btn)
+        
     except:
         pass
     
-    login_email=driver.find_element(by=By.NAME,value="current-email")
-    login_password=driver.find_element(by=By.NAME,value="current-password")
+    # get email and password from the env
+    login_email_btn=driver.find_element(by=By.NAME,value="current-email")
+    login_password_btn=driver.find_element(by=By.NAME,value="current-password")
 
-    login_email.send_keys("username")
-    login_password.send_keys("password")
+    get_username=os.getenv("username")
+    get_password=os.getenv("password")
 
-    submit_button = driver.find_element(By.XPATH, "//button[contains(., 'sign')]")
-    driver.execute_script("arguments[0].click();", submit_button)
+    login_email_btn.send_keys(get_username)
+    time.sleep(2) # add a 2 second gap between adding email and password to bypass bot detection
+    login_password_btn.send_keys(get_password)
+    
     try:
-        error = WebDriverWait(driver, 5).until(
-            EC.presence_of_element_located((By.CLASS_NAME, "message"))
+        submit_button=driver.find_element(By.CLASS_NAME,"signIn")
+        submit_button.click()
+        try:
+            WebDriverWait(driver, 20).until(
+            EC.url_contains("/account")
         )
-        if "incorrect" in error.text.lower():
-            driver.quit()
-            return -1
-    except:
-        pass
+            print("login was sucessfull ( just login)")
+            return True
+            
+        except:
+            print("invalid credentials")
+            return False
+    except Exception as e:
+        print("Login error:", e)
+        return False
 
-    # success
-    return 1
+def download_csv():
+    driver.get("https://monkeytype.com/account")
+    driver.implicitly_wait(20.0)
+    export_btn = driver.find_element(By.XPATH, "//div[contains(text(),'Export CSV')]")
+    if(export_btn.is_displayed()):
+        print("export button found ")
+        # export_btn.click()
+        driver.execute_script("arguments[0].click();", export_btn)
+        print("button was clicked successfully \n waiting for downloading the file")
+        
+        time.sleep(20) # wait for 20 seconds to let the download complete
+        
+        return True
+
+    else:
+        return False
--- main.py
+++ main.py
@@ -1,14 +1,30 @@
 import os
 import login 
-get_user=input("Enter username\n")
-get_pass=input("\nenter password\n")
-def save(get_user,get_pass):
+def save():
+    get_user=input("Enter username\n")
+    get_pass=input("\nenter password\n")
     with open(".env","w") as f:
         f.write(f"username={get_user}\n")
         f.write(f"password={get_pass}")
-        f.close
-save(get_user,get_pass)
-if login.login():
-    print("succesfull")
-else:
-    print("login failed")
+        f.close()
+# save()
+try:
+    
+    if(login.login()):
+        pass
+        if login.download_csv():
+            print("on the accounts page ")
+        else:
+            print("login was unsucessfull")
+    else:
+        print("unsuccessfull")
+except Exception as e:
+    print(e)
+#check downloaded file
+directory_name=r'/home/akshajtiwari/Desktop/typing_predictor/downloaded_files'
+extensions=(".csv")
+for files in os.listdir(directory_name):
+    if files.endswith(extensions):
+        print("csv file found")
+    else:
+        print("file do not exist")
